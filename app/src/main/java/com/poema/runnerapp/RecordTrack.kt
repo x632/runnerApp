8ee808204ef1770e42b5f867ec7558ca631fa8cb
x632 <55179309+x632@users.noreply.github.com>
2020-04-23 13:17:39 +0200

    timer funkar, men laggar 1 sek/ minut

--- app/src/main/java/com/poema/runnerapp/RecordTrack.kt
+++ app/src/main/java/com/poema/runnerapp/RecordTrack.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import java.util.*
 import kotlin.concurrent.timer
 
@@ -12,18 +13,23 @@ class RecordTrack : AppCompatActivity() {
 
     var timerStarted = false
     var timerOn : Timer? = null
-    var halfSeconds = 0
+    var tenthSeconds = 0
     var seconds = 0
     var minutes = 0
     var hours = 0
-    var pressedStart : Boolean = false
+    var strTenths = "0"
+    var strSec = "00:"
+    var strMin = "00:"
+    var strHour = "00:"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record_track)
         val button = findViewById<Button>(R.id.stopbutton)
         button.setOnClickListener {
-            startTimer(false)
+            if (timerOn != null) {
+                startTimer(false)
+            }
             val intent = Intent(this, NamingTrack::class.java)
             startActivity(intent)
 
@@ -34,14 +40,46 @@ class RecordTrack : AppCompatActivity() {
         }
     }
     fun startTimer(pressedStart : Boolean) {
-        if (pressedStart && timerStarted == false) {
-                timerOn = timer(period = 1000) {
-                println("sekunder: $seconds")
-                seconds += 1
+        if (pressedStart && !timerStarted) {
+            timerOn = timer(period = 100) {
+                tenthSeconds += 1
+                if (tenthSeconds > 9) {
+                    seconds += 1
+                    tenthSeconds = 0
+                    strTenths = ".$tenthSeconds"
+                } else {
+                    strTenths = ".$tenthSeconds"
+                }
+                if (seconds > 59) {
+                    seconds = 0
+                    minutes += 1
+                }
+                if (seconds > 9) {
+                    strSec = ":$seconds"
+                } else {
+                    strSec = ":0$seconds"
+                }
+                if (minutes > 59) {
+                    minutes = 0
+                    hours += 1
+                }
+                if (minutes > 9) {
+                    strMin = ":$minutes"
+                } else {
+                    strMin = ":0$minutes"
+                }
+                if (hours > 9) {
+                    strHour = "$hours"
+                } else {
+                    strHour = "0$hours"
+                }
+                val timerText = findViewById<TextView>(R.id.timer)
+                timerText.text = strHour + strMin + strSec + strTenths
                 timerStarted = true
             }
-        } else {
-          timerOn = null
+        }
+        else {
+            timerOn?.cancel()
             timerStarted = false
         }
 
