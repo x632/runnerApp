15fc1879f36c3a7a00d1ccad6d7c0e088ef7ebe2
x632 <55179309+x632@users.noreply.github.com>
2020-05-03 21:33:51 +0200

    Ritat 2 polylines med olika färger och specifika "endCaps" på kartan. Funkar!

--- app/src/main/java/com/poema/runnerapp/MapsActivity.kt
+++ app/src/main/java/com/poema/runnerapp/MapsActivity.kt
@@ -1,28 +1,38 @@
 package com.poema.runnerapp
 
 import android.content.Intent
-import android.location.Location
-import android.location.LocationManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnPolylineClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
 import kotlin.concurrent.timer
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CustomCap
+import com.google.android.gms.maps.model.JointType
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.model.RoundCap
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, OnPolylineClickListener {
 
     private lateinit var mMap: GoogleMap
     var timerStarted = false
     var timerOn : Timer? = null
     var timeUnit = -1
+    private val COLOR_BLACK_ARGB = -0x1000000
+    private val COLOR_GREEN_ARGB = -0xc771c4
+    private val COLOR_RED_ARGB = -0xff000
+    private val PATTERN_DASH_LENGTH_PX = 10
+    private val PATTERN_GAP_LENGTH_PX = 10
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,25 +61,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
     }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Huddinge and move the camera
-        val huddinge = LatLng(59.2351, 17.9973)
-        mMap.addMarker(MarkerOptions().position(huddinge).title("Marker in Huddinge"))
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(59.2351, 17.9973), 16.0f),5000,null)
-
-    }
     fun startTimer(pressedStart : Boolean) {
         if (pressedStart && !timerStarted) {
             timerOn = timer(period = 1000) {
@@ -90,6 +81,102 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             timerOn?.cancel()
             timerStarted = false
         }
+    }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Huddinge and move the camera
+        // Add polylines to the map.
+        // Polylines are useful to show a route or some other connection between points.
+        val polyline1 = googleMap.addPolyline(
+            PolylineOptions()
+                .clickable(false)
+                .add(
+                    LatLng(59.235136, 17.997155),
+                    LatLng(59.235432, 17.997665),
+                    LatLng(59.234730, 17.998588),
+                    LatLng(59.235537, 17.999822),
+                    LatLng(59.235252, 18.001238),
+                    LatLng(59.234775, 18.001978),
+                    LatLng(59.232385, 18.004181),
+                    LatLng(59.232209, 18.001628),
+                    LatLng(59.231452, 18.001099),
+                    LatLng(59.230673, 18.000230),
+                    LatLng(59.230717, 17.998889),
+                    LatLng(59.232144, 17.997752),
+                    LatLng(59.233752, 17.997419),
+                    LatLng(59.234185, 17.998138),
+                    LatLng(59.234712, 17.998578)
+                )
+        )
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline1.tag = "A"
+
+        stylePolyline(polyline1)
+
+        val polyline2 = googleMap.addPolyline(
+            PolylineOptions()
+                .clickable(false)
+                .add(
+                    LatLng(59.235185, 17.997256),
+                    LatLng(59.235432, 17.997665),
+                    LatLng(59.234730, 17.998588),
+                    LatLng(59.235537, 17.999822),
+                    LatLng(59.235252, 18.001238),
+                    LatLng(59.234775, 18.001978)
+
+                )
+        )
+        // Store a data object with the polyline, used here to indicate an arbitrary type.
+        polyline2.tag = "B"
+
+        stylePolyline(polyline2)
+
+
+        val huddinge = LatLng(59.2351, 17.9973)
+       // mMap.addMarker(MarkerOptions().position(huddinge).title("Marker in Huddinge"))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(59.2351, 17.9973), 16.0f),5000,null)
+
+        // Set listeners for click events.
+        googleMap.setOnPolylineClickListener(this)
+
+    }
+
+
+    override fun onPolylineClick(p0: Polyline?) {
+
+    }
+    private fun stylePolyline(polyline: Polyline) {
+        var type = ""
+        // Get the data object stored with the polyline.
+        if (polyline.tag != null) {
+            type = polyline.tag.toString()
+        }
+        when (type) {
+            "A" ->                 // Use a custom bitmap as the cap at the start of the line.
+            {polyline.color = COLOR_GREEN_ARGB
+                polyline.startCap = RoundCap()
+                polyline.endCap = RoundCap()
+            }
+                /*polyline.startCap = CustomCap(
+                    BitmapDescriptorFactory.fromResource(R.drawable.ic_arrow))*/
+            "B" ->                 // Use a round cap at the start of the line.
+            {polyline.color = COLOR_RED_ARGB
+            polyline.startCap = RoundCap()
+                polyline.endCap = CustomCap(
+                    BitmapDescriptorFactory.fromResource(R.drawable.ic_arrow))}
+        }
+        polyline.width = 8.toFloat()
+        polyline.jointType = JointType.ROUND
     }
 }
