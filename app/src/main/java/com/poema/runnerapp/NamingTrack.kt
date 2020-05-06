ce5c1e00260c25b5b8a7eeefaca2634c422311a2
x632 <55179309+x632@users.noreply.github.com>
2020-05-06 18:49:51 +0200

    Gjort klart distansmätningen och visningen och sparningen av den. Lagt till lite restriktioner för hur "backbutton" kan navigera. Lagt till cancelbtn funktionalitet, samt en toast

--- app/src/main/java/com/poema/runnerapp/NamingTrack.kt
+++ app/src/main/java/com/poema/runnerapp/NamingTrack.kt
@@ -6,26 +6,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class NamingTrack : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_naming_track)
 
         val timeUnit = intent.getIntExtra("Time",0 )
+        val distance : Double = intent.getDoubleExtra("Distance", 0.0)
         val resultTimeText = makeTimeStr(timeUnit)
         val saveButton = findViewById<Button>(R.id.save)
         val timeText= findViewById<TextView>(R.id.timeValue)
+        val lengthText = findViewById<TextView>(R.id.textView4)
+        val cancelBtn = findViewById<Button>(R.id.cancel)
+        lengthText.text = String.format("%.0f", distance)+" meters"
         timeText.text = resultTimeText
 
         saveButton.setOnClickListener {
             val intent = Intent(this, TracksActivity::class.java)
             val trackName = getName()
-            intent.putExtra("name", trackName)
-            intent.putExtra("time", timeUnit)
+            intent.putExtra("name2", trackName)
+            intent.putExtra("time2", timeUnit)
+            intent.putExtra("distance2", distance)
+            startActivity(intent)
+        }
+            cancelBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
@@ -39,7 +47,13 @@ class NamingTrack : AppCompatActivity() {
         val hours = timeUnit / 36000
         val minutes = timeUnit % 36000 / 60
         val seconds: Int = timeUnit % 60
-        val resultText = "%1$02d:%2$02d:%3$02d".format(hours, minutes, seconds)
-        return resultText
+        return "%1$02d:%2$02d:%3$02d".format(hours, minutes, seconds)
+    }
+    override fun onBackPressed() {
+            Toast.makeText(getApplicationContext(), "You cannot go back here. You can always delete your track on the next page. Or cancel",
+                Toast.LENGTH_LONG).show();
+
+
+
     }
 }
