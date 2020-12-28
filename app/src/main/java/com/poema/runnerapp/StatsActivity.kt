13e681ac2351dbc2f7000bf799d07353f452ba26
Andreas Svedstedt <andreas@poema.se>
2020-12-28 19:02:05 +0100

    Gjort recyclerView på statistiken

--- app/src/main/java/com/poema/runnerapp/StatsActivity.kt
+++ app/src/main/java/com/poema/runnerapp/StatsActivity.kt
@@ -25,8 +25,6 @@ class StatsActivity : AppCompatActivity(), CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
     lateinit var series: LineGraphSeries<DataPoint>
-    var x: Long = 0
-    var y: Int = 0
     private lateinit var db: AppDatabase
     private var aoTrackId: Long = 0
     private var downloadedAttObjects = mutableListOf<AttemptObject>()
@@ -66,10 +64,8 @@ class StatsActivity : AppCompatActivity(), CoroutineScope {
     fun createGraph() {
 
         val graph: GraphView = findViewById<GraphView>(R.id.graph)
-
         series = LineGraphSeries<DataPoint>()
-
-        series.color = Color.GREEN;
+        series.color = Color.GREEN
         for ((index, attemptObject) in downloadedAttObjects.withIndex()) {
             val x = index + 1.toDouble()
             val y = attemptObject.aoTime.toDouble()
@@ -127,51 +123,6 @@ class StatsActivity : AppCompatActivity(), CoroutineScope {
 
     }
 }
-
-/*
-    private fun writeSomeStuff() {
-        val theText = findViewById<TextView>(R.id.tvAttObj)
-        val theContent = getContent()
-        theText.text = theContent
-    }
-
-    private fun getContent(): String {
-        var fied = "Total number of runs on this track: ${downloadedAttObjects.size}\n\n"
-        fied += "Date and avg speed: (max. last 6)\n"
-
-        for ((index, attemptObject) in downloadedAttObjects.withIndex()) {
-            val temp3 = attemptObject.aoLength.roundToInt()
-            totalLength += temp3
-            if (downloadedAttObjects.size >= 7 && index > (downloadedAttObjects.size-7) || downloadedAttObjects.size < 7) {
-                val temp = attemptObject.aoLength.roundToInt()
-
-                val temp2 = attemptObject.aoTime
-                val result1: Double = temp / temp2.toDouble()
-                val finalResult: Double = (3600 * result1) * 0.001
-                val b: String = attemptObject.aoTimestamp
-                val short = "  " + b.substring(0, b.length - 10)
-                fied += ("$short : " + String.format("%.2f", (finalResult)) + " km/h\n")
-            }
-        }
-
-        fied += "\nTime and length of track: (last 6)\n"
-        for ((index, attemptObject) in downloadedAttObjects.withIndex()) {
-            if (downloadedAttObjects.size >= 7 && index > (downloadedAttObjects.size-7) || downloadedAttObjects.size < 7) {
-                val temp = attemptObject.aoLength.roundToInt()
-                val temp2 = makeTimeStr(attemptObject.aoTime)
-                val b: String = attemptObject.aoTimestamp
-                val short = "  " + b.substring(0, b.length - 10)
-                fied += if (temp2 == currentBest) {
-                    ("$short: $temp2, $temp m (curr. best)\n")
-                } else {
-                    ("$short: $temp2, $temp m\n")
-                }
-            }
-        }
-        fied += ("\nTotal length ran on this track (all runs): ${totalLength} m\n\n")
-        return fied
-    }
-   */
 
 
 
